da7ab49f5fa101e4645fd7c1226e9e787203516c
pratikYesankar <pratikiitkgp585@gmail.com>
2025-04-29 02:23:30 +0530

    novels : frontend part

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
-// import AddNovelForm from "./components/AddNovelForm"
+
 import Novels from "./components/Novels"
 
 function App() {
   return (
     <>
-      {/* <AddNovelForm /> */}
       <Novels />
     </>
   )
